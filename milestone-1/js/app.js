de14c40cc5291e07c357d4e1257116ba4058b39e
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-11-08 11:22:25 +0100

    [add] keyup.enter & v-model but error when i push

--- milestone-1/js/app.js
+++ milestone-1/js/app.js
@@ -4,7 +4,7 @@ createApp({
   data(){
     return{
       activeContact: 0,
-      activeChat: 0,
+      getNewMsg: '',
       contacts: [
         {
           name: 'Michele',
@@ -13,19 +13,19 @@ createApp({
           visible: true,
           messages: [
             {
-            date: '10/01/2020 15:30:55',
-            message: 'Hai portato a spasso il cane?',
-            status: 'sent'
+              date: '10/01/2020 15:30:55',
+              message: 'Hai portato a spasso il cane?',
+              status: 'sent'
             },
             {
-            date: '10/01/2020 15:50:00',
-            message: 'Ricordati di stendere i panni',
-            status: 'sent'
+              date: '10/01/2020 15:50:00',
+              message: 'Ricordati di stendere i panni',
+              status: 'sent'
             },
             {
-            date: '10/01/2020 16:15:22',
-            message: 'Tutto fatto!',
-            status: 'received'
+              date: '10/01/2020 16:15:22',
+              message: 'Tutto fatto!',
+              status: 'received'
             }
           ],
         },
@@ -36,19 +36,19 @@ createApp({
           visible: true,
           messages: [
             {
-            date: '20/03/2020 16:30:00',
-            message: 'Ciao come stai?',
-            status: 'sent'
+              date: '20/03/2020 16:30:00',
+              message: 'Ciao come stai?',
+              status: 'sent'
             },
             {
-            date: '20/03/2020 16:30:55',
-            message: 'Bene grazie! Stasera ci vediamo?',
-            status: 'received'
+              date: '20/03/2020 16:30:55',
+              message: 'Bene grazie! Stasera ci vediamo?',
+              status: 'received'
             },
             {
-            date: '20/03/2020 16:35:00',
-            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-            status: 'sent'
+              date: '20/03/2020 16:35:00',
+              message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+              status: 'sent'
             }
           ],
         },
@@ -59,19 +59,19 @@ createApp({
           visible: true,
           messages: [
             {
-            date: '28/03/2020 10:10:40',
-            message: 'La Marianna va in campagna',
-            status: 'received'
+              date: '28/03/2020 10:10:40',
+              message: 'La Marianna va in campagna',
+              status: 'received'
             },
             {
-            date: '28/03/2020 10:20:10',
-            message: 'Sicuro di non aver sbagliato chat?',
-            status: 'sent'
+              date: '28/03/2020 10:20:10',
+              message: 'Sicuro di non aver sbagliato chat?',
+              status: 'sent'
             },
             {
-            date: '28/03/2020 16:15:22',
-            message: 'Ah scusa!',
-            status: 'received'
+              date: '28/03/2020 16:15:22',
+              message: 'Ah scusa!',
+              status: 'received'
             }
           ],
         },
@@ -82,14 +82,14 @@ createApp({
           visible: true,
           messages: [
             {
-            date: '10/01/2020 15:30:55',
-            message: 'Lo sai che ha aperto una nuova pizzeria?',
-            status: 'sent'
+              date: '10/01/2020 15:30:55',
+              message: 'Lo sai che ha aperto una nuova pizzeria?',
+              status: 'sent'
             },
             {
-            date: '10/01/2020 15:50:00',
-            message: 'Si, ma preferirei andare al cinema',
-            status: 'received'
+              date: '10/01/2020 15:50:00',
+              message: 'Si, ma preferirei andare al cinema',
+              status: 'received'
             }
           ],
         },
@@ -100,14 +100,14 @@ createApp({
           visible: true,
           messages: [
             {
-            date: '10/01/2020 15:30:55',
-            message: 'Ricordati di chiamare la nonna',
-            status: 'sent'
+              date: '10/01/2020 15:30:55',
+              message: 'Ricordati di chiamare la nonna',
+              status: 'sent'
             },
             {
-            date: '10/01/2020 15:50:00',
-            message: 'Va bene, stasera la sento',
-            status: 'received'
+              date: '10/01/2020 15:50:00',
+              message: 'Va bene, stasera la sento',
+              status: 'received'
             }
           ],
         },
@@ -118,19 +118,19 @@ createApp({
           visible: true,
           messages: [
             {
-            date: '10/01/2020 15:30:55',
-            message: 'Ciao Claudia, hai novità?',
-            status: 'sent'
+              date: '10/01/2020 15:30:55',
+              message: 'Ciao Claudia, hai novità?',
+              status: 'sent'
             },
             {
-            date: '10/01/2020 15:50:00',
-            message: 'Non ancora',
-            status: 'received'
+              date: '10/01/2020 15:50:00',
+              message: 'Non ancora',
+              status: 'received'
             },
             {
-            date: '10/01/2020 15:51:00',
-            message: 'Nessuna nuova, buona nuova',
-            status: 'sent'
+              date: '10/01/2020 15:51:00',
+              message: 'Nessuna nuova, buona nuova',
+              status: 'sent'
             }
           ],
         },
@@ -141,14 +141,14 @@ createApp({
           visible: true,
           messages: [
             {
-            date: '10/01/2020 15:30:55',
-            message: 'Fai gli auguri a Martina che è il suo compleanno!',
-            status: 'sent'
+              date: '10/01/2020 15:30:55',
+              message: 'Fai gli auguri a Martina che è il suo compleanno!',
+              status: 'sent'
             },
             {
-            date: '10/01/2020 15:50:00',
-            message: 'Grazie per avermelo ricordato, le scrivo subito!',
-            status: 'received'
+              date: '10/01/2020 15:50:00',
+              message: 'Grazie per avermelo ricordato, le scrivo subito!',
+              status: 'received'
             }
           ],
         },
@@ -159,19 +159,19 @@ createApp({
           visible: true,
           messages: [
             {
-            date: '10/01/2020 15:30:55',
-            message: 'Ciao, andiamo a mangiare la pizza stasera?',
-            status: 'received'
+              date: '10/01/2020 15:30:55',
+              message: 'Ciao, andiamo a mangiare la pizza stasera?',
+              status: 'received'
             },
             {
-            date: '10/01/2020 15:50:00',
-            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-            status: 'sent'
+              date: '10/01/2020 15:50:00',
+              message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+              status: 'sent'
             },
             {
-            date: '10/01/2020 15:51:00',
-            message: 'OK!!',
-            status: 'received'
+              date: '10/01/2020 15:51:00',
+              message: 'OK!!',
+              status: 'received'
             }
           ],
         }
@@ -181,6 +181,15 @@ createApp({
   methods:{
     changeContact(index){
       this.activeContact = index
+    },
+    printMsg(){
+      const newMsg = {
+        date:'',
+        message: this.getNewMsg,
+        status: 'sent'
+      }
+      this.contacs.messages.push(newMsg)
+      console.log(newMsg.message);
     }
   }
 }).mount('#app')
